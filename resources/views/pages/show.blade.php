a03c332fd825ba9e838003fa0520bc5c7ef47c05
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-21 12:11:51 +0100

    test show page

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,6 +1,14 @@
-@extends('layouts.app')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
 
-<h1>Show</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-@endsection
+<body>
+    <h1>Show page</h1>
+</body>
+
+</html>
